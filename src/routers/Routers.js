577c9608df5d90a611cb276b8571220f58a6e235
wasim <wasim1daoud97@gmail.com>
2023-05-27 18:52:44 +0300

    fixing home page

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -13,7 +13,7 @@ const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="home" />} />
-      <Route path="/Ecommerse" element={<Home />} />
+      <Route path="/Ecommerse/" element={<Home />} />
       <Route path="home" element={<Home />} />
       <Route path="cart" element={<Cart />} />
       <Route path="shop" element={<Shop />} />
